57b65bc3ec5ddafc911148eb77601ca7df6cdeb8
Raghav Aggarwal <f1e2b@ugrad.cs.ubc.ca>
2018-09-17 22:38:43 -0700

    Week 2 Requirements fulfilled. Project idea changed to Word Meaning tracker for novels. Implemented Book and Word classes.

---
+++ src/ui/PlayingWithWords.java
@@ -0,0 +1,47 @@
+package ui;
+
+import models.Book;
+import models.Word;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PlayingWithWords {
+
+    private static void printLongWordMeanings(ArrayList<Word> words) {
+        for (Word word: words) {
+            if (word.getWord().length() > 6) {
+                System.out.println(word.getWordMeaning());
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Word genial = new Word("genial", "friendly and cheerful");
+        Word innocuous = new Word("innocuous", "not harmful or offensive");
+        Word vehement = new Word("vehement", "showing strong feeling");
+       // System.out.println(genial.getWordMeaning());
+
+        Book theBookThief = new Book("The Book Thief");
+        theBookThief.insertWord(genial);
+        theBookThief.insertWord(innocuous);
+        theBookThief.insertWord(vehement);
+
+        ArrayList<Word> theBookThiefWords = theBookThief.getWords();
+
+        printLongWordMeanings(theBookThiefWords);
+
+        Scanner user_input = new Scanner(System.in);
+
+        System.out.print("Enter word: ");
+        String new_word = user_input.next();
+
+        System.out.print("Enter meaning: ");
+        String new_meaning = user_input.next();
+
+        theBookThief.insertWord(new Word(new_word, new_meaning));
+        printLongWordMeanings(theBookThief.getWords());
+
+    }
+}
